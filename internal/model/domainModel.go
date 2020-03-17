0abb939508e61a5f31bf77af49892e027444cc2a
martin bergqlin <martin@ourstudio.se>
2020-03-17 09:02:16 +0100

    updated with geolocation

--- internal/model/domainModel.go
+++ internal/model/domainModel.go
@@ -2,14 +2,13 @@ package model
 
 // Location represents a location (...)
 type Location struct {
-	ID        string  `json:"id"`
-	Name      string  `json:"name"`
-	Longitude float64 `json:"longitude"`
-	Latitude  float64 `json:"latitude"`
-	City      string  `json:"city"`
-	Country   string  `json:"country"`
-	Street    string  `json:"street"`
-	Zip       string  `json:"zip"`
+	ID       string      `json:"id"`
+	Name     string      `json:"name"`
+	City     string      `json:"city"`
+	Country  string      `json:"country"`
+	Street   string      `json:"street"`
+	Zip      string      `json:"zip"`
+	Location GeoLocation `json:"geolocation"`
 }
 
 // Organization ...
@@ -33,12 +32,19 @@ type User struct {
 
 // Logg ...
 type Logg struct {
-	User     User      `json:"user"`
-	Symptoms []Symptom `json:"symptoms"`
+	User     User        `json:"user"`
+	Symptoms []Symptom   `json:"symptoms"`
+	Location GeoLocation `json:"geolocation"`
 }
 
 // Symptom ...
 type Symptom struct {
 	Type  string `json:"type"`
 	Value int    `json:"value"`
+}
+
+// GeoLocation ...
+type GeoLocation struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lon"`
 }
