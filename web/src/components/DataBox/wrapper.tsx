1925f4ea1d5286f4f9cce6bf283f09eca6c73472
Alexander Erlandsson <alexander@ourstudio.se>
2020-03-18 10:25:54 +0100

    Updates

--- web/src/components/DataBox/wrapper.tsx
+++ web/src/components/DataBox/wrapper.tsx
@@ -3,21 +3,18 @@ import styled from 'styled-components';
 import { size } from '../../layout/helpers';
 
 export const Wrapper = styled.div`
-  display: flex;
-  align-items: flex-start;
   background-color: ${(props) => props.theme.color.primaryLight};
   padding: ${size(2)};
   border-radius: 4px;
 `;
 
 export const Label = styled.div`
-  flex: 1 1 auto;
   font-weight: 700;
+  font-size: 0.875rem;
 `;
 
 export const Value = styled.div`
-  flex-shrink: 0;
-  text-align: right;
+
 `;
 
 export const ValueMain = styled.div`
