b0bc661fe832cd5db44a59872e8d28e55b7c7716
Alexander Erlandsson <alexander@ourstudio.se>
2020-03-19 22:06:34 +0100

    Added frontend for verify pages

--- web/src/routes/user/verify/index.tsx
+++ web/src/routes/user/verify/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 
+import Page from '../../../components/Page';
+import Container from '../../../components/Container';
+import Repeat from '../../../components/Repeat';
+import Content from '../../../components/Content';
+import Loader from '../../../components/Loader';
+import Link from '../../../components/Link';
+import Snackbar from '../../../components/Snackbar';
+import { H1 } from '../../../components/Heading';
+
 import { TrackView } from '../../../utils/tracking';
 import useUserVerify from './useUserVerify';
 
@@ -16,17 +25,54 @@ const UserVerify = ({ match }: UserVerify): JSX.Element => {
   React.useEffect(() => {
     TrackView()
   }, []);
-  
+
   if (successCreate) {
-    return <p>Verifiering lyckades</p>
+    return (
+      <Page>
+        <Container textCenter>
+          <Repeat large>
+            <Content center>
+              <H1>Din e-postadress är nu bekräftad</H1>
+              <p>Välkommen till Viruskoll! Snart kommer du få den första frågan skickad till din e-postadress. Sedan kommer samma fråga dyka upp dagligen. Ditt svar bidrar till ett bättre underlag för Viruskoll.se.</p>
+            </Content>
+          </Repeat>
+          <Repeat large>
+            <Link to="/">Gå till startsidan</Link>
+          </Repeat>
+        </Container>
+      </Page>
+    );
   }
 
   if (failedCreate) {
-    return <p>Verifiering misslyckades</p>
+    return (
+      <Page>
+        <Container textCenter>
+          <Repeat large>
+            <Snackbar
+              severity="error"
+              heading="Något gick fel..."
+              icon={true}
+            >
+              Din e-postadress kunde inte bekräftas. Vänligen försök igen.
+            </Snackbar>
+          </Repeat>
+          <Repeat large>
+            <Link to="/">Gå till startsidan</Link>
+          </Repeat>
+        </Container>
+      </Page>
+    );
   }
 
   if (creating) {
-    return <p>Verifierar...</p>
+    return (
+      <Page>
+        <Container>
+          <Loader />
+        </Container>
+      </Page>
+    );
   }
 
   return null;
