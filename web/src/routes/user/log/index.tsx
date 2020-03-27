fc74deca2d93043b01d570dae8990f6e2a7f7457
Carl Pihl <carl@ourstudio.se>
2020-03-27 10:19:15 +0100

    Rename log routes and corresponding components according to new log properties

--- web/src/routes/user/log/index.tsx
+++ web/src/routes/user/log/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
-import NoSymptom from './no-symptom';
-import WorkFromHome from './work-from-home';
-import ChildCare from './child-care';
-import HomeNoWork from './home-no-work';
-import HasSymptom from './has-symptom';
+import Normal from './normal';
+import Abnormal from './abnormal';
 import useTrackView from '../../../hooks/useTrackView';
 
+const NORMAL_STATE = 'normal';
+const ABNORMAL_STATE = 'abnormal';
+
+// TODO: Old log types below. To be removed short after release of the ones above.
 const NO_SYMPTOM = 'no-symptom';
 const WORK_FROM_HOME = 'work-from-home';
 const CHILD_CARE = 'child-care';
 const HOME_NO_WORK = 'home-no-work';
 const HAS_SYMPTOM = 'has-symptom';
+// End of old log types.
 
 interface Props {
   id: string;
@@ -29,16 +31,20 @@ const Log = ({ match }: Log): JSX.Element | null => {
   }
 
   switch (type.toLowerCase()) {
+    case NORMAL_STATE:
+      return <Normal id={id} />;
+    case ABNORMAL_STATE:
+      return <Abnormal id={id} />;
+
+    // TODO: Old log types below. To be removed short after release of the ones above.
     case NO_SYMPTOM:
-      return <NoSymptom id={id} />;
+      return <Normal id={id} />;
     case WORK_FROM_HOME:
-      return <WorkFromHome id={id} />;
     case CHILD_CARE:
-      return <ChildCare id={id} />;
     case HOME_NO_WORK:
-      return <HomeNoWork id={id} />;
     case HAS_SYMPTOM:
-      return <HasSymptom id={id} />;
+      return <Abnormal id={id} />;
+    // End of old log types.
     default:
       return null;
   }
