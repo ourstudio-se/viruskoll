c39d5448858277222d79e4a5bcaae3661307869d
martin bergqlin <martin@ourstudio.se>
2020-03-28 18:59:04 +0100

    updated domainmodel

--- internal/model/domainModel.go
+++ internal/model/domainModel.go
@@ -48,23 +48,32 @@ type GeoLocation struct {
 	Longitude float64 `json:"lon"`
 }
 
-type GeoAggBucket struct {
-	GeoJsonId string `json:"geoJsonId"`
-	DocCount  int64  `json:"doc_count"`
-}
 type GeoAgg struct {
 	Buckets []GeoAggBucket `json:"buckets"`
+}
+
+type LogSearchResults struct {
+	Count          int64            `json:"count"`
+	GeoLocations   []*GeoAggBucket  `json:"geolocations"`
+	Unhealthy      []*SymptomBucket `json:"unhealthy"`
+	Healthy        []*SymptomBucket `json:"healthy"`
+	WorkSituations []*SymptomBucket `json:"workingSituation"`
+}
+
+type GeoAggBucket struct {
+	ID            string       `json:"id"`
+	Count         int64        `json:"count"`
+	Unhealthy     *SymptomsAgg `json:"unhealthy"`
+	Healthy       *SymptomsAgg `json:"healthy"`
+	WorkSituation *SymptomsAgg `json:"workingSituation"`
+}
+
+type SymptomsAgg struct {
+	Count   int64            `json:"count"`
+	Buckets []*SymptomBucket `json:"buckets"`
 }
 
 type SymptomBucket struct {
 	Symptom interface{} `json:"symptom"`
 	Count   int64       `json:"count"`
-}
-
-type SymptomsAgg struct {
-	Count          int64           `json:"count"`
-	Unhealthy      []SymptomBucket `json:"unhealthy"`
-	Healthy        []SymptomBucket `json:"healthy"`
-	WorkSituations []SymptomBucket `json:"workingSituations"`
-	GeoLocations   []GeoAggBucket  `json:"geolocations"`
 }
