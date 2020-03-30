adf4127f5fd5113bb64d693c9cce5ee3726c721a
Alexander Erlandsson <alexander@ourstudio.se>
2020-03-30 10:53:27 +0200

    Fixed lint

--- web/src/components/Gdpr/gdpr-confirm.tsx
+++ web/src/components/Gdpr/gdpr-confirm.tsx
@@ -22,7 +22,11 @@ const GdprConfirm = ({ gdpr, onGdprChange }: GdprConfirm): JSX.Element => {
           onChange={onGdprChange}
         >
           <div>
-            Jag samtycker till att Viruskoll behandlar min e-post adress, den grupp jag valt att tillhöra och de loggar jag delar med mig av kring min arbetssituation och mina symptom för att kunna sammanställa och i anonymiserat skick visa ett underlag på många människors situation för allmänheten på viruskoll.se.{' '}
+            Jag samtycker till att Viruskoll behandlar min e-post adress, den
+            grupp jag valt att tillhöra och de loggar jag delar med mig av kring
+            min arbetssituation och mina symptom för att kunna sammanställa och
+            i anonymiserat skick visa ett underlag på många människors situation
+            för allmänheten på viruskoll.se.{' '}
             <ButtonInline onClick={onDisplayGdpr}>
               Läs mer om hur Viruskoll hanterar dina personuppgifter här
             </ButtonInline>
