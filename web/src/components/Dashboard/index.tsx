ff2a22feedbf59a879722a991e662e547702a4df
Alexander Erlandsson <alexander@ourstudio.se>
2020-03-30 16:38:21 +0200

    Fixed map in safari

--- web/src/components/Dashboard/index.tsx
+++ web/src/components/Dashboard/index.tsx
@@ -20,15 +20,17 @@ export const Dashboard = styled.main`
 `;
 
 export const DashboardMap = styled.div`
+  flex: 1 1 auto;
   position: relative;
   background-color: ${(props) => props.theme.color.accent};
 
   ${(props) => props.theme.breakpoint.LtMd} {
-    flex: 1 1 auto;
-  }
+    display: flex;
+    flex-direction: column;
 
-  ${(props) => props.theme.breakpoint.Md} {
-    flex: 1 1 auto;
+    > * {
+      flex: 1 1 auto;
+    }
   }
 `;
 
