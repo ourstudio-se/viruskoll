4a36d89fd0f02ec50ddbc33e30fb772b2c4d4fbc
Peter <peter@ourstudio.se>
2020-04-01 18:55:15 +0200

    working on colors

--- web/src/components/Virus/map-utils/options.ts
+++ web/src/components/Virus/map-utils/options.ts
@@ -1,5 +1,8 @@
+import { MapStyle } from './styles';
+
 export const MapOptions: google.maps.MapOptions = {
   fullscreenControl: false,
   streetViewControl: false,
   mapTypeControl: false,
+  styles: MapStyle,
 };
--- web/src/components/Virus/map-utils/styles.ts
+++ web/src/components/Virus/map-utils/styles.ts
@@ -1,15 +1,121 @@
+export const MapColors = [
+  '#13D380',
+  '#0EBA6F',
+  '#0AA260',
+  '#06874F',
+  '#08653C',
+  '#064F2F',
+  '#064429',
+  '#043B23',
+  '#042F1C',
+  '#022114',
+  '#01190F',
+];
+
 export const dataLayerStyle: google.maps.Data.StyleOptions = {
-  fillColor: '#a0ead3',
-  fillOpacity: 0.5,
-  strokeColor: '#161e2e',
+  fillColor: MapColors[0],
+  fillOpacity: 0.8,
+  strokeColor: '#000000',
   strokeOpacity: 1,
   strokeWeight: 0.5,
 };
 
 export const styleMouseOver: google.maps.Data.StyleOptions = {
-  strokeWeight: 2,
+  strokeColor: '#000000',
+  strokeWeight: 3,
 };
 
 export const styleMouseOut: google.maps.Data.StyleOptions = {
+  strokeColor: '#000000',
   strokeWeight: dataLayerStyle.strokeWeight,
 };
+
+export const MapStyle: google.maps.MapTypeStyle[] = [
+  {
+    featureType: 'administrative',
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        color: '#444444',
+      },
+    ],
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'all',
+    stylers: [
+      {
+        color: '#f2f2f2',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    elementType: 'all',
+    stylers: [
+      {
+        saturation: -100,
+      },
+      {
+        lightness: 45,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'simplified',
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'water',
+    elementType: 'all',
+    stylers: [
+      {
+        color: '#dbdbdb',
+      },
+      {
+        visibility: 'on',
+      },
+    ],
+  },
+];
